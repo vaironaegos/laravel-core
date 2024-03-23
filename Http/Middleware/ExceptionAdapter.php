bec2e51efe6b9013e3715e32ea6fcda2e34a9f9b
Moisés Kalebe <moises979@gmail.com>
2024-03-23 10:46:56 -0300

    feat: QueryException json error

--- Http/Middleware/ExceptionAdapter.php
+++ Http/Middleware/ExceptionAdapter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Astrotech\Core\Laravel\Http\Middleware;
 
 use Closure;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Astrotech\Core\Base\Exception\ExceptionBase;
@@ -24,6 +25,18 @@ final class ExceptionAdapter
                     'trace' => !app()->environment('production') ? $response->exception->getTrace() : []
                 ],
             ])->setStatusCode($response->exception->getStatusCode());
+        }
+
+        if ($response->exception instanceof QueryException) {
+            return response()->json([
+                'status' => 'error',
+                'data' => $response->exception->getBindings(),
+                'meta' => [
+                    'message' => $response->exception->getMessage(),
+                    'sql' => $response->exception->getSql(),
+                    'trace' => !app()->environment('production') ? $response->exception->getTrace() : []
+                ],
+            ])->setStatusCode(500);
         }
 
 

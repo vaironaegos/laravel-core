28475ecbf8af48af93f0738935f982dc825021ea
Moisés Kalebe <moises979@gmail.com>
2024-03-23 10:47:19 -0300

    feat: Validator implements @base validation interface

--- Validation/LaravelValidator.php
+++ Validation/LaravelValidator.php
@@ -4,60 +4,49 @@ declare(strict_types=1);
 
 namespace Astrotech\Core\Laravel\Validation;
 
-use Astrotech\Core\Base\Adapter\Contracts\Dto;
+use Astrotech\Core\Base\Adapter\Contracts\ValidatorInterface;
 use Astrotech\Core\Base\Exception\ValidationException;
 use Illuminate\Support\Facades\Validator as FacadesValidator;
 
-final class LaravelValidator
+final class LaravelValidator implements ValidatorInterface
 {
     private array $errors = [];
 
-    public function validate(array $values, array $rules): bool
+    public static function validate(string $field, mixed $value, string $validationRule): void
     {
-        return $this->isValid();
-    }
-
-    public function validateDto(Dto $dto): bool
-    {
-        return $this->validate($dto->values(), $dto->rules());
-    }
-
-    public function make(
-        array $data,
-        array $rules,
-        array $messages = [],
-        array $customAttributes = []
-    ): void {
-
         $validator = FacadesValidator::make(
-            $data,
-            $rules,
-            $messages,
-            $customAttributes
+            [$field => $value],
+            [$field => $validationRule],
         );
 
         if ($validator->fails()) {
             $errors = $validator->errors()->toArray();
             foreach ($errors as $field => $message) {
-                $value = $data[$field] ?? null;
-                $this->errors = ['field' => $field,
+                throw new ValidationException([
+                    'field' => $field,
                     'error' => underscoreToCamelCase(str_replace('.', '_', $message[0])),
-                    'value' => $value];
-
-                if (!$this->isValid()) {
-                    throw new ValidationException($this->getErrors());
-                }
+                    'value' => $value
+                ]);
             }
         }
     }
 
-    public function isValid(): bool
+    public static function validateBatch(array $values, array $validationRules): void
     {
-        return count($this->errors) === 0;
-    }
+        $validator = FacadesValidator::make(
+            $values,
+            $validationRules,
+        );
 
-    public function getErrors(): array
-    {
-        return $this->errors;
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            foreach ($errors as $field => $message) {
+                throw new ValidationException([
+                    'field' => $field,
+                    'error' => underscoreToCamelCase(str_replace('.', '_', $message[0])),
+                    'value' => $values[$field]
+                ]);
+            }
+        }
     }
 }

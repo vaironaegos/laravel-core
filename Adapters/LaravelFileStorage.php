c2eec3c5e85fcf6ae74cf3e569dc1beb47208ef7
Moisés Kalebe <moises979@gmail.com>
2024-03-26 20:44:46 -0300

    feat: Laravel File Storage

---
+++ Adapters/LaravelFileStorage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Astrotech\Core\Laravel\Adapters;
+
+use Astrotech\Core\Base\Adapter\Contracts\FileStorage;
+
+class LaravelFileStorage implements FileStorage
+{
+    public function store(string $destinationPath, string $newName = ''): string
+    {
+        if (empty($newName)) {
+            $newName = time();
+        }
+
+        $file = current(request()->allFiles());
+        if (!$file) {
+            return '';
+        }
+
+        $fileName = $newName . '.' . $file->extension();
+        $file->storeAs($destinationPath, $fileName, 'public');
+
+        return $fileName;
+    }
+}

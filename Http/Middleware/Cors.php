b5b133a00e2e8c07999163f932756c7798141957
Moisés Kalebe <moises979@gmail.com>
2024-03-27 09:09:27 -0300

    feat: Cors middleware

---
+++ Http/Middleware/Cors.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Astrotech\Core\Laravel\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+final class Cors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', "*")
+            ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
+            ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
+    }
+}

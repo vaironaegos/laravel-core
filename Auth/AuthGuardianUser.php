d45a69462567849ec4e16b49af8995c3fdfd9a6f
Kilderson Sena <dersonsena@gmail.com>
2024-07-05 14:58:17 -0300

    feat: implementation of Auth Guardian Middleware to authenticate clients

---
+++ Auth/AuthGuardianUser.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Astrotech\Core\Laravel\Auth;
+
+use Exception;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property string $login
+ * @property array $group
+ * @property array $permissions
+ */
+final class AuthGuardianUser implements Authenticatable
+{
+    private array $userInfo;
+
+    public function __construct(array $userInfo)
+    {
+        $this->userInfo = $userInfo;
+    }
+
+    public function __get(string $name): mixed
+    {
+        if (!array_key_exists($name, $this->userInfo)) {
+            throw new Exception("Invalid user field '{$name}'");
+        }
+
+        return $this->userInfo[$name];
+    }
+
+    public function getAuthIdentifierName(): string
+    {
+        return 'id';
+    }
+
+    public function getAuthIdentifier(): string
+    {
+        return $this->userInfo['id'];
+    }
+
+    public function getAuthPassword(): ?string
+    {
+        return null;
+    }
+
+    public function getRememberToken(): ?string
+    {
+        return null;
+    }
+
+    public function setRememberToken($value)
+    {
+    }
+
+    public function getRememberTokenName(): ?string
+    {
+        return null;
+    }
+
+    public function getAuthPasswordName()
+    {
+    }
+}
---
+++ Http/Middleware/AuthGuardianMiddleware.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Astrotech\Core\Laravel\Http\Middleware;
+
+use Closure;
+use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\RequestException;
+use Astrotech\Core\Laravel\Auth\AuthGuardianUser;
+
+final class AuthGuardianMiddleware
+{
+    protected string $authGuardianUrl;
+    protected GuzzleClient $guzzleClient;
+
+    public function __construct()
+    {
+        $this->authGuardianUrl = config('services.authGuardian.baseUrl');
+        $this->guzzleClient = new GuzzleClient();
+    }
+
+    public function handle(Request $request, Closure $next)
+    {
+        $token = $request->bearerToken();
+
+        if (!$token) {
+            return response()->json(['error' => 'Token not provided'], 401);
+        }
+
+        try {
+            $response = $this->guzzleClient->get($this->authGuardianUrl . '/users/identity', [
+                'headers' => [
+                    'Authorization' => "Bearer {$token}",
+                ]
+            ]);
+
+            if ($response->getStatusCode() !== 200) {
+                return response()->json(['error' => 'Invalid token'], 401);
+            }
+
+            $userInfo = json_decode((string)$response->getBody(), true);
+            Auth::setUser(new AuthGuardianUser($userInfo['data']));
+
+            return $next($request);
+        } catch (RequestException|Throwable $e) {
+            return response()->json(['error' => 'Invalid token'], 401);
+        }
+    }
+}

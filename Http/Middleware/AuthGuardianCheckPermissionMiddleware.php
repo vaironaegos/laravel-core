a34b68a1cce2f8a343544b9e40d6c7bc71317edb
Italo André <italoaaoliveira@gmail.com>
2024-08-30 15:40:19 -0300

    chore: refactors auth guardian permission middleware

--- Http/Middleware/AuthGuardianCheckPermissionMiddleware.php
+++ Http/Middleware/AuthGuardianCheckPermissionMiddleware.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Astrotech\Core\Laravel\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client as GuzzleClient;
 
@@ -22,26 +23,22 @@ final class AuthGuardianCheckPermissionMiddleware
     public function handle(Request $request, Closure $next, string $key)
     {
         $token = $request->bearerToken();
-
         if (!$token) {
             return response()->json(['error' => 'Token not provided'], 401);
         }
-
-        $response = $this->guzzleClient->get($this->authGuardianUrl . '/users/has-permission?key=' . $key, [
-            'headers' => [
-                'Authorization' => "Bearer {$token}",
-            ]
-        ]);
-
-        if ($response->getStatusCode() !== 200) {
-            return response()->json(['error' => 'permissionDenied'], 401);
-        }
-
-        $userInfo = json_decode((string)$response->getBody(), true);
-        if (!$userInfo['data']['hasPermission']) {
+        try {
+            $response = $this->guzzleClient->get($this->authGuardianUrl . '/users/has-permission?key=' . $key, [
+                'headers' => [
+                    'Authorization' => "Bearer {$token}",
+                ]
+            ]);
+            $userInfo = json_decode((string)$response->getBody(), true);
+            if (!$userInfo['data']['hasPermission']) {
+                return response()->json(['error' => 'permissionDenied'], 403);
+            }
+            return $next($request);
+        } catch (Exception $e) {
             return response()->json(['error' => 'permissionDenied'], 403);
         }
-
-        return $next($request);
     }
 }

ae08ab41812701b789f0f99d33059e8b5f806d02
Kilderson Sena <kilderson@astrotech.solutions>
2024-10-29 09:48:21 -0300

    feat: creating NewAuthorizationMiddleware with external_id field as key

---
+++ Http/Middleware/NewAuthorizationMiddleware.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Astrotech\Core\Laravel\Http\Middleware;
+
+use Astrotech\Core\Base\Exception\ValidationException;
+use Astrotech\Core\Laravel\Http\HttpStatus;
+use Closure;
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Http\Request;
+use UnexpectedValueException;
+use Firebase\JWT\SignatureInvalidException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+
+final class NewAuthorizationMiddleware
+{
+    /**
+     * Handle an incoming request.
+     * @param \Illuminate\Http\Request $request
+     * @param Closure $next
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $token = $request->headers->get('Authorization');
+
+            if (!$token) {
+                throw new ValidationException(
+                    details: ['error' => 'missingAuthorizationHeader'],
+                    code: HttpStatus::UNAUTHORIZED->value
+                );
+            }
+
+            $token = trim(explode(' ', $token)[1]);
+            $payload = JWT::decode($token, new Key(config('jwt.keys.public'), config('jwt.algo')));
+            $user = User::firstWhere('external_id', $payload->sub);
+
+            if (!$user) {
+                return response()->json(['status' => 'User not found'], 404);
+            }
+
+            auth('api')->login($user);
+        } catch (SignatureInvalidException $e) {
+            return response()->json(['status' => 'Invalid Signature'], 403);
+        } catch (TokenExpiredException $e) {
+            return response()->json(['status' => 'Token is Expired'], 403);
+        } catch (UnexpectedValueException $e) {
+            return response()->json(['status' => 'Token is Invalid'], 403);
+        }
+
+        return $next($request);
+    }
+}

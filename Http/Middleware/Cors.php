521093cd9e8e3fda373d341cad79f305933ed300
Kilderson Sena <dersonsena@gmail.com>
2024-11-21 16:47:09 -0300

    feat: improvement of cors middleware

--- Http/Middleware/Cors.php
+++ Http/Middleware/Cors.php
@@ -11,11 +11,60 @@ final class Cors
 {
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', $request->header('Origin') ?? '*')
-            ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
-            ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type")
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Expose-Headers', 'Set-Cookie');
+        $origin = $request->header('Origin');
+        $allowedOrigins = [$origin];
+
+        if (config('app.cors.allowed_origins') !== null) {
+            $allowedOrigins = config('app.cors.allowed_origins');
+        }
+
+        if (!$origin || !in_array($origin, $allowedOrigins)) {
+            return response()->json(['error' => 'originNotAllowed'], 403);
+        }
+
+        $allowedHeaders = [
+            "Ngrok-Skip-Browser-Warning",
+            "Accept",
+            "Authorization",
+            "Cache-Control",
+            "Content-Type",
+            "DNT",
+            "If-Modified-Since",
+            "Keep-Alive",
+            "Origin",
+            "User-Agent",
+            "X-Requested-With",
+            "Bearer",
+            "Device",
+            "Context",
+            "Context-Secondary",
+            "PrivateToken",
+            "Meta"
+        ];
+
+        $exposedHeaders = [
+            "Set-Cookie",
+            "Ngrok-Skip-Browser-Warning",
+            "Authorization",
+            "Bearer",
+            "Device",
+            "Context",
+            "Context-Secondary",
+            "PrivateToken",
+            "Meta"
+        ];
+
+        $response = $next($request)
+            ->header('Access-Control-Allow-Origin', $origin)
+            ->header('Access-Control-Allow-Methods', "PUT, PATCH, HEADERS, POST, DELETE, GET, OPTIONS")
+            ->header('Access-Control-Allow-Headers', implode(",", $allowedHeaders))
+            ->header('Access-Control-Allow-Credentials', "true")
+            ->header('Access-Control-Expose-Headers', implode(",", $exposedHeaders));
+
+        if ($request->getMethod() === 'OPTIONS') {
+            $response->setStatusCode(204);
+        }
+
+        return $response;
     }
 }

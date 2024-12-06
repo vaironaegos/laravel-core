10ae0a05825e29202e99efed9d980d6324917237
David Amorim <davi.amorin2@gmail.com>
2024-12-06 13:43:00 -0300

    chore:Refactoring the use of AuthGuardianApi

--- AuthGuardian/Api/AuthGuardianApi.php
+++ AuthGuardian/Api/AuthGuardianApi.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Astrotech\Core\Laravel\AuthGuardian\Api;
 
 use GuzzleHttp\Client as GuzzleClient;
+use Illuminate\Http\Request;
 
 final class AuthGuardianApi
 {
@@ -22,51 +23,81 @@ final class AuthGuardianApi
         $this->headers[$name] = $value;
     }
 
-    public function createUser(array $data): array
+    public function getGroupId(string $groupName, string $token, Request $request): array
     {
-        $response = $this->guzzleClient->post($this->baseUrl . '/users/with-password', [
+        $response = $this->executeRequest('GET', $this->baseUrl . '/groups', [
+            'headers' => [
+                ...$this->headers,
+                'Authorization' => "Bearer {$token}",
+                'Origin' => $request->header('Origin'),
+            ],
+            'query' => [
+                'skipPagination' => 1,
+                'filter[name][like]' => $groupName,
+            ],
+        ]);
+
+
+        if (!isset($response['data']) || count($response['data']) !== 1) {
+            return ['error' => 'couldNotDefineGroupId'];
+        }
+
+        return ['id' => $response['data'][0]['id']];
+    }
+
+    private function executeRequest(string $method, string $url, array $options): array
+    {
+        $response = $this->guzzleClient->{$method}($url, $options);
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function createUser(array $data, Request $request): array
+    {
+        $response = $this->executeRequest('POST', $this->baseUrl . '/users/with-password', [
             'json' => $data,
             'auth' => [$this->clientId, $this->clientSecret],
-            'headers' => $this->headers
+            'headers' =>[
+                ...$this->headers,
+                'Origin' => $request->header('Origin'),
+            ],
         ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return $response;
     }
 
-    public function deleteUser(string $userId, string $token): array
+    public function deleteUser(string $userId, string $token,Request $request): array
     {
-        $response = $this->guzzleClient->delete($this->baseUrl . '/users/' . $userId, [
+        return $this->executeRequest('DELETE',$this->baseUrl . '/users/' . $userId, [
             'headers' => [
                 ...$this->headers,
                 'Authorization' => "Bearer {$token}",
+                'Origin' => $request->header('Origin'),
             ]
         ]);
-
-        return json_decode($response->getBody()->getContents(), true);
     }
 
-    public function destroyUser(string $userId, string $token): array
+    public function destroyUser(string $userId, string $token, Request $request): array
     {
-        $response = $this->guzzleClient->delete($this->baseUrl . "/users/{$userId}/destroy", [
+        return $this->executeRequest('DELETE',$this->baseUrl .
+            "/users/{$userId}/destroy", [
             'headers' => [
                 ...$this->headers,
                 'Authorization' => "Bearer {$token}",
+                'Origin' => $request->header('Origin'),
             ]
         ]);
-
-        return json_decode($response->getBody()->getContents(), true);
     }
 
-    public function updateUser(string $userId, array $data, string $token): array
+    public function updateUser(string $userId, array $data, string $token,string $origin): array
     {
-        $response = $this->guzzleClient->put($this->baseUrl . "/users/{$userId}", [
+
+        return $this->executeRequest('PUT',$this->baseUrl . "/users/{$userId}", [
             'json' => $data,
             'headers' => [
                 ...$this->headers,
                 'Authorization' => "Bearer {$token}",
+                'Origin' => $origin,
             ]
         ]);
-
-        return json_decode($response->getBody()->getContents(), true);
     }
 }

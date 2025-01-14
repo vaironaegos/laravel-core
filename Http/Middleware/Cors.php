651f146d0a5b37061ddb24da2d5955b7acd3f6ea
Kilderson Sena <dersonsena@gmail.com>
2025-01-14 14:46:55 -0300

    chore: handling StreamedResponse response type

--- Http/Middleware/Cors.php
+++ Http/Middleware/Cors.php
@@ -6,12 +6,13 @@ namespace Astrotech\Core\Laravel\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 final class Cors
 {
     public function handle(Request $request, Closure $next)
     {
-        $origin = $request->header('Origin');
+        $origin = $request->headers->get('Origin');
         $allowedOrigins = [$origin];
 
         if (config('app.cors.allowed_origins') !== null) {
@@ -54,8 +55,17 @@ final class Cors
             "Meta"
         ];
 
-        $response = $next($request)
-            ->header('Access-Control-Allow-Origin', $origin)
+        $response = $next($request);
+
+        if ($response instanceof StreamedResponse) {
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Access-Control-Allow-Methods', "PUT, PATCH, HEADERS, POST, DELETE, GET, OPTIONS");
+            $response->headers->set('Access-Control-Allow-Credentials', "true");
+            $response->headers->set('Access-Control-Expose-Headers', implode(",", $exposedHeaders));
+            return $response;
+        }
+
+        $response->header('Access-Control-Allow-Origin', $origin)
             ->header('Access-Control-Allow-Methods', "PUT, PATCH, HEADERS, POST, DELETE, GET, OPTIONS")
             ->header('Access-Control-Allow-Headers', implode(",", $allowedHeaders))
             ->header('Access-Control-Allow-Credentials', "true")
